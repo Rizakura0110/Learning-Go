55556b13fbe5c4314b0f09dd1ce0dacf23f8bd5b
Rizakura0110 <rasakura0110@gmail.com>
2022-11-03 23:50:29 +0900

    2-4

--- 2-3/main.go
+++ 2-3/main.go
@@ -18,10 +18,19 @@ func main() {
 	fmt.Println(y)
 
 	// エラー
-	x = x + 1
-	y = "bye"
+	/*
+		x = x + 1
+		y = "bye"
 
-	fmt.Println(x)
-	fmt.Println(y)
+		fmt.Println(x)
+		fmt.Println(y)*/
+
+	// Goの定数はリテラルに名前を付けるもの
+
+	// 変数に代入された値が使われなくてもエラーにはならない
+	apple := 10
+	apple = 20
+	fmt.Println(apple)
+	apple = 30
 
 }
