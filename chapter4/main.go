095667bf7708fa8c3f673297f040065647f758ca
Rizakura0110 <rasakura0110@gmail.com>
2022-11-09 23:54:55 +0900

    4-3

--- chapter4/main.go
+++ chapter4/main.go
@@ -37,4 +37,92 @@ func main() { //liststart
 		}
 		// エラー fmt.Println(n)
 	}
+
+	// for
+	{
+		for i := 0; i < 10; i++ {
+			fmt.Println(i)
+		}
+	}
+
+	{
+		i := 1
+		for i < 100 {
+			fmt.Println(i)
+			i = i * 2
+		}
+	}
+
+	{
+		for i := 1; i <= 100; i++ {
+			if i%3 == 0 && i%5 == 0 {
+				fmt.Println(i, "は3でも5でも割り切れる")
+				continue
+			}
+			if i%3 == 0 {
+				fmt.Println(i, "は3で割り切れる")
+				continue
+			}
+			if i%5 == 0 {
+				fmt.Println(i, "は5で割り切れる")
+				continue
+			}
+			fmt.Println(i, "は3でも5でも割り切れない")
+		}
+	}
+
+	{
+		evenVals := []int{2, 4, 6, 8, 10, 12}
+		for i, v := range evenVals {
+			fmt.Println(i, v)
+		}
+	}
+
+	{
+		evenVals := []int{2, 4, 6, 8, 10, 12}
+		for _, v := range evenVals {
+			fmt.Println(v)
+		}
+	}
+
+	{
+		unique := map[string]bool{"花子": true, "太郎": true, "洋子": true}
+		for k := range unique {
+			fmt.Println(k)
+		}
+	}
+
+	{
+		m := map[string]int{
+			"a": 1,
+			"c": 3,
+			"b": 2,
+		}
+		for i := 0; i < 3; i++ {
+			fmt.Println("ループ", i)
+			for k, v := range m {
+				fmt.Println(k, v)
+			}
+		}
+	}
+
+	{
+		evenVals := []int{2, 4, 6, 8, 10}
+		for i, v := range evenVals {
+			if i == 0 {
+				continue
+			}
+			fmt.Println(i, v)
+			if i == len(evenVals)-2 {
+				break
+			}
+		}
+	}
+
+	{
+		evenVals := []int{2, 4, 6, 8, 10}
+		for i := 1; i < len(evenVals)-1; i++ {
+			fmt.Println(i, evenVals[i])
+		}
+	}
 }
