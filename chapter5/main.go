8cac825c1cbad94d3fc092fe2e27528959fb24fe
Rizakura0110 <rasakura0110@gmail.com>
2022-11-14 23:45:15 +0900

    5-3-1

--- chapter5/main.go
+++ chapter5/main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"sort"
 )
 
 func div(numerator int, denominator int) int {
@@ -141,4 +142,31 @@ func main() {
 		}(i)
 	}
 	*/
+	// クロージャ
+	type Person struct {
+		FirstName string
+		LastName  string
+		Age       int
+	}
+	people := []Person{
+		{"Pat", "Patterson", 37},
+		{"Tracy", "Bobbert", 23},
+		{"Fred", "Fredson", 18},
+	}
+	fmt.Println("●初期データ")
+	fmt.Println(people)
+	sort.Slice(people, func(i int, j int) bool {
+		return people[i].LastName < people[j].LastName
+	})
+	fmt.Println("●姓（LastName。2番目のフィールド）でソート")
+	fmt.Println(people)
+	sort.Slice(people, func(i int, j int) bool {
+		return people[i].Age < people[j].Age
+	})
+	fmt.Println("●年齢（Age）でソート")
+	fmt.Println(people)
+
+	fmt.Println("●ソート後のpeople") //liststart4
+	fmt.Println(people)
+
 }
