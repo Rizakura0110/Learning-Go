4a8ed717dc23ad1999609267bf6bca8ca02f8fdd
Rizakura0110 <rasakura0110@gmail.com>
2022-11-17 00:22:22 +0900

    6-4

--- chapter6/main.go
+++ chapter6/main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+func failedupdate(g *int) {
+	x := 10
+	g = &x
+}
+func failedupdate2(px *int) {
+	x2 := 20
+	px = &x2
+}
+func update(px *int) {
+	*px = 20
+}
+
+type Foo struct {
+	Failed1 string
+	Failed2 int
+}
+
+func makeFoo() (Foo, error) {
+	f := Foo{
+		Failed1: "val1",
+		Failed2: 20,
+	}
+	return f, nil
+}
+
 func main() {
 	x := 10
 	pointerToX := &x
@@ -38,4 +63,16 @@ func main() {
 	}
 	fmt.Println(p)
 	fmt.Println(*p.MiddleName)
+
+	var f *int
+	failedupdate(f)
+	fmt.Println(f)
+
+	x2 := 10
+	failedupdate2(&x2)
+	fmt.Println(x)
+	update(&x)
+	fmt.Println(x)
+
+	_, _ = makeFoo()
 }
