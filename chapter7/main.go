529e78f5fe609fdc0db55db74adf06561bf41356
Rizakura0110 <rasakura0110@gmail.com>
2022-11-19 17:30:57 +0900

    7-2-5

--- chapter7/main.go
+++ chapter7/main.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+/*
 type Person struct {
 	LastName  string
 	FirstName string
@@ -13,7 +14,7 @@ type Person struct {
 
 func (p Person) String() string {
 	return fmt.Sprintf("%s %s：年齢%d歳", p.LastName, p.FirstName, p.Age)
-}
+}*/
 
 type Counter struct {
 	total       int
@@ -39,18 +40,105 @@ func doUpdateRight(c *Counter) {
 	fmt.Println("OK:", c.String())
 }
 
-func main() {
-	p := Person{
-		LastName:  "武田",
-		FirstName: "信玄",
-		Age:       52,
+type IntTree struct {
+	val         int
+	left, right *IntTree
+}
+
+func (it *IntTree) Insert(val int) *IntTree {
+	if it == nil {
+		return &IntTree{val: val}
 	}
-	output := p.String()
-	fmt.Println(output)
+	if val < it.val {
+		it.left = it.left.Insert(val)
+	} else if val > it.val {
+		it.right = it.right.Insert(val)
+	}
+	return it
+}
+
+func (it *IntTree) Contains(val int) bool {
+	switch {
+	case it == nil:
+		return false
+	case val < it.val:
+		return it.left.Contains(val)
+	case val > it.val:
+		return it.right.Contains(val)
+	default:
+		return true
+	}
+}
+
+type Adder struct {
+	start int
+}
+
+func (a Adder) AddTo(val int) int {
+	return a.start + val
+}
+
+type Score int
+type HighScore Score
+
+type Person struct {
+	LastName  string
+	FirstName string
+	Age       int
+}
+
+type Employee Person
+
+func (s Score) Double() Score {
+	return s * 2
+}
+
+func main() {
+	/*
+		p := Person{
+			LastName:  "武田",
+			FirstName: "信玄",
+			Age:       52,
+		}
+		output := p.String()
+		fmt.Println(output)
+	*/
 
 	var c Counter
 	doUpdateWrong(c)
 	fmt.Println("main:", c.String())
 	doUpdateRight(&c)
 	fmt.Println("main:", c.String())
+
+	var it *IntTree
+	it = it.Insert(5)
+	it = it.Insert(3)
+	it = it.Insert(10)
+	it = it.Insert(2)
+	fmt.Println(it.Contains(2))  // true
+	fmt.Println(it.Contains(12)) // false
+
+	myAdder10 := Adder{start: 10}
+	fmt.Println(myAdder10.AddTo(5))
+
+	f1 := myAdder10.AddTo
+	fmt.Println(f1(10))
+
+	f2 := Adder.AddTo
+	fmt.Println(f2(myAdder10, 15))
+
+	var i int = 300
+	var s Score = 100
+	var hs HighScore = 200
+	s = Score(i)
+	hs = HighScore(s)
+	fmt.Println(s, hs)
+
+	hhs := hs + 20
+	fmt.Println(hhs)
+
+	s = 200
+	hs = 300
+	fmt.Println(s.Double())
+	fmt.Println(Score(hs).Double())
 }
