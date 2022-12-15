237f41768c7252da803f3163ed5694efff0932d4
Rizakura0110 <rasakura0110@gmail.com>
2022-12-16 00:26:51 +0900

    11-5

--- chapter11/main.go
+++ chapter11/main.go
@@ -192,39 +192,65 @@ func main() {
 		fmt.Printf("%+v\n", data)
 	}
 	/*
-		{
-			s := http.Server{
-				Addr:         ":8080",
-				ReadTimeout:  30 * time.Second,
-				WriteTimeout: 90 * time.Second,
-				IdleTimeout:  120 * time.Second,
-				Handler:      HelloHandler{},
-			}
-			fmt.Println("ブラウザで http://localhost:8080/ を開いてください。")
-			err := s.ListenAndServe()
-			if err != nil {
-				if err != http.ErrServerClosed {
-					panic(err)
+			{
+				s := http.Server{
+					Addr:         ":8080",
+					ReadTimeout:  30 * time.Second,
+					WriteTimeout: 90 * time.Second,
+					IdleTimeout:  120 * time.Second,
+					Handler:      HelloHandler{},
+				}
+				fmt.Println("ブラウザで http://localhost:8080/ を開いてください。")
+				err := s.ListenAndServe()
+				if err != nil {
+					if err != http.ErrServerClosed {
+						panic(err)
+					}
 				}
 			}
+
+		{
+			personMux := http.NewServeMux()
+			personMux.HandleFunc("/greet",
+				func(w http.ResponseWriter, r *http.Request) {
+					w.Write([]byte("こんにちは！\n"))
+				},
+			)
+			dogMux := http.NewServeMux()
+			dogMux.HandleFunc("/greet",
+				func(w http.ResponseWriter, r *http.Request) {
+					w.Write([]byte("カワイイ子犬ちゃんだね！\n"))
+				},
+			)
+			mux := http.NewServeMux()
+			mux.Handle("/person/", http.StripPrefix("/person", personMux))
+			mux.Handle("/dog/", http.StripPrefix("/dog", dogMux))
+			fmt.Printf("%s", "次でテスト:\ncurl http://localhost:8080/dog/greet\ncurl http://localhost:8080/person/greet\n")
+			log.Fatal(http.ListenAndServe(":8080", mux))
 		}
 	*/
 	{
-		personMux := http.NewServeMux()
+		personMux := http.NewServeMux() //liststart1
 		personMux.HandleFunc("/greet",
 			func(w http.ResponseWriter, r *http.Request) {
 				w.Write([]byte("こんにちは！\n"))
-			},
-		)
+			})
+
 		dogMux := http.NewServeMux()
 		dogMux.HandleFunc("/greet",
 			func(w http.ResponseWriter, r *http.Request) {
 				w.Write([]byte("カワイイ子犬ちゃんだね！\n"))
-			},
-		)
+			})
 		mux := http.NewServeMux()
 		mux.Handle("/person/", http.StripPrefix("/person", personMux))
 		mux.Handle("/dog/", http.StripPrefix("/dog", dogMux))
+
+		terribleSecurity := TerribleSecurityProvider("GOPHER")
+		mux.Handle("/hello", terribleSecurity(RequestTimer(
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("Hello!\n"))
+			}),
+		)))
 		fmt.Printf("%s", "次でテスト:\ncurl http://localhost:8080/dog/greet\ncurl http://localhost:8080/person/greet\n")
 		log.Fatal(http.ListenAndServe(":8080", mux))
 	}
@@ -391,4 +417,30 @@ type HelloHandler struct{}
 
 func (hh HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello!\n"))
+}
+
+func RequestTimer(h http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			h.ServeHTTP(w, r)
+			end := time.Now()
+			log.Printf("request time for %s: %v", r.URL.Path, end.Sub(start))
+		},
+	)
+}
+
+var securityMsg = []byte("You didn't give the secret password\n")
+
+func TerribleSecurityProvider(password string) func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Header.Get("X-Secret-Password") != password {
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write(securityMsg)
+				return
+			}
+			h.ServeHTTP(w, r)
+		})
+	}
 }
